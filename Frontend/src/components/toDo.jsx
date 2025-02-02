ad8b323cb80b4cd713446f4eca7b3593581c424d
Saksham <apple@apples-MacBook-Pro.local>
2025-02-02 15:09:32 +0530

    Done with cancel button

--- Frontend/src/components/toDo.jsx
+++ Frontend/src/components/toDo.jsx
@@ -12,6 +12,7 @@ const toDo = () => {
         priority:'',
         fulfillment:50,
     });
+    const [cancel,setCancel]=useState(false);
     function change(event)
     {
         const {name,value}=event.target;
@@ -32,10 +33,15 @@ const toDo = () => {
             console.error("Error saving task",error);
         }
      }
+     function cancelClicked()
+     {
+        setCancel(true);
+     }
     
     
   return (
     <>
+    {cancel && window.location.reload()}
     <div className='w-[1100px] h-[529px] bg-white rounded-[20px] pt-[30px] mt-[20px] ml-[50px] pl-[50px]'>
                 <h1 className='h1-jost text-[32px] w-[234px] h-[46px]'>Add a new to-do:</h1>
                 <form className='mt-[30px] flex flex-col md:flex-row'>
@@ -70,7 +76,7 @@ const toDo = () => {
                             <input onChange={change} type="range" name="fulfillment" id="" value={formData.fulfillment} className='w-[330px] h-[33px] border-solid border-2 border-black mt-[34px]' />
                         </div>
                          <button onClick={saveTask} type="submit" className='mt-[155px] h1-jost bg-customBlue w-[215px] h-[60px] text-[24px] text-white rounded-[20px] ml-[54px]'>Save</button>
-                         <button type="button" className='border-2 border-black w-[215px] h-[60px] rounded-[20px] ml-[21px] h1-jost'>Cancel</button>
+                         <button onClick={cancelClicked} type="button" className='border-2 border-black w-[215px] h-[60px] rounded-[20px] ml-[21px] h1-jost'>Cancel</button>
                     </div>
                    
                 </form>
